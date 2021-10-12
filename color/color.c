a6756d49696e124ade36bcd0a7b7200d2ec4d37d
Imanolasolo <jjusturi@Usuario.localdomain>
2021-10-12 16:13:35 -0500

    color

---
+++ color/color.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char name[20];
+    char color[20];
+    printf("What is your name?");
+    scanf("%s",name);
+    printf("What is your favorite color?");
+    scanf("%s",color);
+    printf("%s´s favorite color is %s\n",name,color);
+    return(0);
+}
